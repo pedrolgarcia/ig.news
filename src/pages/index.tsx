4d09da3cc3e869bcde834c5a16319591b7ac0d06
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-11-15 16:25:02 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,34 @@
 import Head from 'next/head'
+import Image from 'next/image'
+
+import { SubscribeButton } from '../components/SubscribeButton'
+
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <Image src="/images/avatar.svg" alt="" width={334} height={520} />
+      </main>
     </>
   )
 }
