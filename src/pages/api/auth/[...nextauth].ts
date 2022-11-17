ed5a093047b5e388582113c7880cace24a858ad1
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-11-17 00:22:49 -0300

    feat: create if not exists

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
 
 export const authOptions = {
   providers: [
@@ -13,6 +16,26 @@ export const authOptions = {
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user }: any) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email))),
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email))),
+          ),
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  },
 }
 
 export default NextAuth(authOptions)
